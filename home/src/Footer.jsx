b3f486a6d792ae35edbfc5596214a354fd42f82f
Ahmed-Gelle <tiko.gelle13@gmail.com>
2023-04-12 14:02:38 +0200

    Exposed Header and Footer Components

---
+++ home/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">We sell best Spinner World</div>
+  )
+}
+
+export default Footer
